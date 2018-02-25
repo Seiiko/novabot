681619b174179a6755cae9bbc775f9eb25307ad6
Seiiko <seiko.eleanor@gmail.com>
2018-02-25 23:03:02 +0000

    1.0.0: Added the .noticeme command.

--- cmd/interaction/noticeme.js
+++ cmd/interaction/noticeme.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
 
     // Send the info message to the current channel.
     message.channel.send(`:kissing_heart:  |  <@!` + message.author.id + `> wants to be noticed by <@!` + member.user.id + `>!`, {
-        file: "http://24.media.tumblr.com/071c399ec79c91b0165c5a01ce2fb6dc/tumblr_mj9wk0hg7a1ra3xu6o1_500.gif"
+        file: "https://i.imgur.com/y5pLsKy.gif"
     });
 
 }
