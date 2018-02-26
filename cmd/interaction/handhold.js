004d648d876608d7799a623b590a812057c5d33b
Seiiko <seiko.eleanor@gmail.com>
2018-02-26 16:38:26 +0000

    1.0.0: Fixed the .handhold command.

--- cmd/interaction/handhold.js
+++ cmd/interaction/handhold.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
     let member = message.mentions.members.first();
 
     // Send the info message to the current channel.
-    message.channel.send(`:shrug:  |  <@!` + message.author.id + `> and  <@!` + member.user.id + `> are holding hands!`, {
+    message.channel.send(`:handshake:  |  <@!` + message.author.id + `> and  <@!` + member.user.id + `> are holding hands!`, {
         file: "https://media.giphy.com/media/ZaaGHJGe4QJyM/giphy.gif"
     });
 
