1739a8b872737ce4109fdd5148349754ddfe7125
Seiiko <seiko.eleanor@gmail.com>
2018-02-26 16:41:53 +0000

    1.0.0: Added the .barf command.

--- cmd/interaction/barf.js
+++ cmd/interaction/barf.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
     let member = message.mentions.members.first();
 
     // Send the info message to the current channel.
-    message.channel.send(`:handshake:  |  <@!` + message.author.id + `> is barfing at  <@!` + member.user.id + `>!`, {
+    message.channel.send(`:nauseated_face:  |  <@!` + message.author.id + `> is barfing at  <@!` + member.user.id + `>!`, {
         file: "http://i0.kym-cdn.com/photos/images/original/001/110/533/dff.gif"
     });
 
