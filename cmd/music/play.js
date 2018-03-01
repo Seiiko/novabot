2acf86a15e7eee94e3177aa7e821f14dfdfe2816
Seiiko <seiko.eleanor@gmail.com>
2018-03-01 22:06:10 +0000

    Aha.

--- cmd/music/play.js
+++ cmd/music/play.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
     if(!voiceChannel) // Check if the member is connected to a voice channel.
         return message.channel.send(":sound:  |  You need to be in a Voice Channel to play music!");
 
-    const songInfo = await ytdl.getInfo(args[0]);
+    var songInfo = await ytdl.getInfo(args[0]);
 
     // PLAY THE SONG
     if(!serverQueue) { // Create queue if there's none.
