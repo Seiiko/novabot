e82b0500e831ed86afe7c4f6452d2eefeb9cdc22
Seiiko <seiko.eleanor@gmail.com>
2018-03-11 11:22:08 +0000

    Polishing.

--- cmd/owner/test.js
+++ cmd/owner/test.js
@@ -12,42 +12,46 @@ module.exports.run = async (client, message, args) => {
       const embedCommands = new Discord.RichEmbed() // Create a constant referring to the embed message.
 
       .setTitle('Check out Sei\'s YouTube channel.')
-      .setAuthor('Nova   |   Help', 'https://cdn.discordapp.com/attachments/394292753330339840/408696752489627668/luminus_bot_icon.png')
+      .setAuthor('Nova   |   Help', 'https://cdn.discordapp.com/attachments/419222687525896202/422352880985440257/luminus_bot_stars.png')
   
-      .setColor('#8462b2')
+      .setColor('#7d4586')
       .setDescription('For help with a specific command, type \'.help [command]\'.')
   
-      .setFooter('Nova, the universal magic bot. Made by Sei.', 'https://cdn.discordapp.com/attachments/394292753330339840/408696752489627668/luminus_bot_icon.png')
+      .setFooter('Nova, the universal magic bot. Made by Sei.', 'https://cdn.discordapp.com/attachments/419222687525896202/422352880985440257/luminus_bot_stars.png')
   
       .setURL('https://www.youtube.com/c/seiitunes')
+
+      .addBlankField(true)
   
       .addField('Regular Commands',
-      '.avatar  |  .help  |  .info  |  .ping  |  .report')
+      '.avatar   |   .help   |   .info   |   .ping   |   .report')
 
       .addBlankField(true)
   
       .addField('Fun Commands',
-      '.8ball  |  .catfact  |  .coinflip  |  .dieroll  |  .dogfact  |  .repeat  |  .motiv  |  .potato  |  .puppy  |  .rate')
+      '.8ball   |   .catfact  |   .coinflip   |   .dieroll   |   .dogfact  |   .repeat   |   .motiv   |   .potato   |   .puppy   |   .rate')
 
       .addBlankField(true)
   
       .addField('Interaction Commands',
-      '.barf  |  .cuddle  |  .handhold  |  .hug  |  .kiss  |  .noticeme  |  .pat  |  .poke  |  .pout  |  .punch  |  .shrug  |  .slap  |  .tickle')
+      '.barf   |   .cuddle   |   .handhold   |   .hug   |   .kiss   |   .noticeme   |   .pat   |   .poke   |   .pout   |   .punch   |   .shrug   |   .slap   |   .tickle')
 
       .addBlankField(true)
   
       .addField('NSFW Commands',
-      '.boobs  |  .butt  |  .e621  |  .gelbooru  |  .hentai  |  .konachan  |  .rule34  |  .vagina')
+      '.boobs   |   .butt   |   .e621   |   .gelbooru   |   .hentai   |   .konachan   |   .rule34   |   .vagina')
 
       .addBlankField(true)
   
       .addField('Admin Commands',
-      '.ban  |  .kick  |  .mute  |  .purge  |  .say  |  .unmute')
+      '.ban   |   .kick   |   .mute   |   .purge   |   .say   |   .unmute')
 
       .addBlankField(true)
       
       .addField('Bot Owner Commands',
-      '.navatar  |  .nick  |  .status  |  .utag')
+      '.navatar   |   .nick   |   .status   |   .utag')
+
+      .addBlankField(true)
   
     // Sending the messages.
     message.member.send(embedCommands);
