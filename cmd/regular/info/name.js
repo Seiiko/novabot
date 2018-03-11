2d7fa749423b279051be198b18bfc5e9269248fc
Seiiko <seiko.eleanor@gmail.com>
2018-03-11 14:39:58 +0000

    Hue.

---
+++ cmd/regular/info/name.js
@@ -0,0 +1,6 @@
+module.exports.ban = ".ban"
+module.exports.kick = ".kick"
+module.exports.mute = ".mute"
+module.exports.purge = ".purge"
+module.exports.say = ".say"
+module.exports.unmute = ".unmute"
