617cb20f654e6d3732cf149f64af589b0b96d3a5
Seiiko <seiko.eleanor@gmail.com>
2018-03-11 21:23:13 +0000

    AHa

---
+++ cmd/regular/iam.js
@@ -0,0 +1,27 @@
+module.exports.run = async (client, message, args) => {
+
+    // Define the mutee and muted role variables.
+    let user = message.member;
+    let nRole = message.guild.roles.find("name", "★ NSFW ★");
+
+    if(args[0] === 'NSFW'){
+
+        // If the member already has the muted role, send a warning message.
+        if(user.roles.has(nRole.id))
+        return message.channel.send(":warning:  |  You already have the NSFW role!");
+
+        // Mute the member.
+        await user.addRole(nRole);
+        message.channel.send(`:white_check_mark:  |  You now have access to the NSFW channel.`);
+    
+    }
+
+
+}
+
+module.exports.info = {
+
+    // Set the command name.
+    name: "iam"
+
+}
